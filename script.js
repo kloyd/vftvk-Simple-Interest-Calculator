1eede8b49ca39177386a3ae983f542cf448eb6d7
kelly loyd <kelly.loyd@icloud.com>
2022-01-05 16:34:22 +0000

    Bug fixes

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 function compute()
 {
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate / 100;
-    var year = new Date().getFullYear() + parseInt(years);
-
     var result = document.getElementById("result");
-    result.innerText = "If you deposit " + principal + ", at an interest rate of " + rate +
-    "You will receive an amount of " + interest + ", in the year " + year;
-    
+    var principal = document.getElementById("principal").value;
+    if (isNaN(principal) || principal < 1) {
+        alert("Please enter a positive number for Principal.");
+        document.getElementById("principal").focus();
+        result.innerHTML = "";
+    } else {
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate / 100;
+       
+        var year = new Date().getFullYear() + parseInt(years);
+
+        result.innerHTML = "If you deposit " + principal + ",<br\>at an interest rate of " + rate +
+        "%<br\>You will receive an amount of " + interest + ", <br\>in the year " + year + ".<br\>";
+    }
 }
 
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText= rateval + "% ";
 }
         
